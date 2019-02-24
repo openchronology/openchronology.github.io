e7b0f1468195df884af0b80be32f0c355646d76b
Athan Clark <athan.clark@gmail.com>
2019-02-24 14:59:33 -0700

    production flag

--- build.sh
+++ build.sh
@@ -13,7 +13,15 @@ pulp build && \
     echo "Bundled" && \
     ./node_modules/.bin/browserify $JSTMP -o $JS -t [ babelify --presets [ @babel/preset-env ] --plugins [ @babel/plugin-proposal-class-properties ] ] && \
     rm $JSTMP && \
-    ./node_modules/.bin/browserify $JS -g [ envify --NODE_ENV production ] -g uglifyify | ./node_modules/.bin/uglifyjs --compress --mangle > $JSMIN && \
+    if [ $# -eq 0 ]; then
+        JSOUT=$JS
+    elif [ $1 == "production" ]; then
+        ./node_modules/.bin/browserify $JS -g [ envify --NODE_ENV production ] -g uglifyify | ./node_modules/.bin/uglifyjs --compress --mangle > $JSMIN && \
+        JSOUT=$JSMIN
+    else
+        echo "incorrect flag usage - either \"production\" or none"
+        exit
+    fi
     echo "Browserified" && \
-    ltext "$TEMPLATE $JSMIN" --raw $JSMIN > $OUTPUT && \
+    ltext "$TEMPLATE $JSOUT" --raw $JSOUT > $OUTPUT && \
     echo "Finished"
