78c0b597d5583af24609d6a3c694c70e0cde2c87
Athan Clark <athan.clark@gmail.com>
2019-02-24 15:13:20 -0700

    early termination for bad args

--- build.sh
+++ build.sh
@@ -8,19 +8,22 @@ TEMPLATE=./build/index.template.html
 
 OUTPUT=./index.html
 
+if [ $# -eq 0 ]; then
+    JSOUT=$JS
+elif [ $1 == "production" ]; then
+    JSOUT=$JSMIN
+else
+    echo "incorrect flag usage - either \"production\" or none"
+    exit
+fi
+
 pulp build && \
     purs bundle output/**/*.js -m Main --main Main > $JSTMP && \
     echo "Bundled" && \
     ./node_modules/.bin/browserify $JSTMP -o $JS -t [ babelify --presets [ @babel/preset-env ] --plugins [ @babel/plugin-proposal-class-properties ] ] && \
     rm $JSTMP && \
-    if [ $# -eq 0 ]; then
-        JSOUT=$JS
-    elif [ $1 == "production" ]; then
-        ./node_modules/.bin/browserify $JS -g [ envify --NODE_ENV production ] -g uglifyify | ./node_modules/.bin/uglifyjs --compress --mangle > $JSMIN && \
-        JSOUT=$JSMIN
-    else
-        echo "incorrect flag usage - either \"production\" or none"
-        exit
+    if [ $# -eq 1 ] && [ $1 == "production" ]; then
+        ./node_modules/.bin/browserify $JS -g [ envify --NODE_ENV production ] -g uglifyify | ./node_modules/.bin/uglifyjs --compress --mangle > $JSMIN || { exit 1; }
     fi
     echo "Browserified" && \
     ltext "$TEMPLATE $JSOUT" --raw $JSOUT > $OUTPUT && \
