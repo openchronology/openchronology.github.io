a7d24b088ecd6f8d8cd149aaa0756b8f2505b087
Athan Clark <athan.clark@gmail.com>
2019-03-03 18:07:43 -0700

    storable equations

--- src/Timeline/Data/StoredTimeline.purs
+++ src/Timeline/Data/StoredTimeline.purs
@@ -3,14 +3,27 @@ module Timeline.Data.StoredTimeline where
 
 newtype StoredTimeline = StoredTimeline
   { timelineName :: String
-  , events :: Array Event
-  , axis :: 
+  , root :: Array Timelines -- ^ Root timeline
+  , axis :: Axis
   }
 
 
-newtype Event = Event
-  { title :: String
-  , description :: String -- Markdown?
-  , start :: Number -- post translation
-  , end :: Number
+
+data TimelineAlignment
+  = TimelineInline
+  | TimelineBox
+
+newtype Timeline = Timeline
+  { title       :: String
+  , description :: String
+  , alignment   :: TimelineAlignment
+  , start       :: Number
+  , end         :: Number
+  , children    :: Array Timeline
+  }
+
+
+newtype Axis = Axis
+  { translation :: Number <-> BigNumber -- ? Units?
+  , description :: String
   }
