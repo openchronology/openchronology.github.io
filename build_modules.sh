f26aaaa7404e197b86aba4fda31fcae2a4297a3a
Athan Clark <athan.clark@gmail.com>
2020-04-29 07:00:05 -0700

    cleaner design

--- build_modules.sh
+++ build_modules.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
-find src -name '*.purs' | xargs graphmod -q > modules.dot
-sed -i 's/size="6,4"/size="12,8"/' modules.dot
+# build graph
+find src -name '*.purs' | xargs graphmod -q -R 'MaterialUI' -d 12,8 > modules.dot
+# rename graph
+sed -i 's/digraph G/digraph Modules/' modules.dot
+# redirect nodes - "is imported by"
 sed -i 's/\(.*\) -> \(.*\);/\2 -> \1;/' modules.dot
+# draw graph to svg
 dot -Tsvg modules.dot -o modules.svg
