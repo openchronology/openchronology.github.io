3cba2713009ed6555387871f6736ad750277de91
Athan Clark <athan.clark@gmail.com>
2020-05-04 03:09:53 -0700

    integrated into npm test
    weird inconsistency with travis
    
    more weirdness with travis
    
    im dumb
    
    maybe missing dependencies?
    
    trying with manual capabilities
    
    trying other options
    
    using chrome options

--- itest/test.js
+++ itest/test.js
@@ -1,12 +1,26 @@
 require('chromedriver');
 const assert = require('assert');
-const {Builder, Key, By, until} = require('selenium-webdriver');
+const {Builder, Key, By, until, Capabilities} = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+
+// let chromeCapabilities = Capabilities.chrome();
+const chromeOptions = new chrome.Options();
+chromeOptions.addArguments('--headless');
+chromeOptions.addArguments('--no-sandbox');
+//   {
+//   'args': ['--headless', '--no-sandbox', '--disable-dev-shm-usage']
+// };
+// chromeCapabilities.set('chromeOptions', chromeOptions);
 
 describe('Testing OpenChronology', function () {
   let driver;
 
   before(async function() {
-    driver = await new Builder().forBrowser('chrome').build();
+    driver = await new Builder()
+               .forBrowser('chrome')
+               .setChromeOptions(chromeOptions)
+              // .withCapabilities(chromeCapabilities)
+               .build();
   });
 
   it('Should Have The Title', async function() {
